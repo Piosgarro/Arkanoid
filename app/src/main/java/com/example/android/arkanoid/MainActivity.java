9db5b20fc821e3ef4c74a34604859df01eceed42
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2020-12-09 16:36:01 +0100

    Arkanoid: Implement animation background in Main Menu
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- app/src/main/java/com/example/android/arkanoid/MainActivity.java
+++ app/src/main/java/com/example/android/arkanoid/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.android.arkanoid;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AnimationDrawable frameAnimation;
     private Game game;
-    private UpdateThread myThread;
     private Handler updateHandler;
+    private UpdateThread myThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,19 @@ public class MainActivity extends AppCompatActivity {
         game = new Game(this, 3, 0);
 
         setContentView(R.layout.activity_main);
+
+        // Carica l'ImageView che ospiterà l'animazione e setta
+        // lo sfondo attraverso la nostra risorsa XML gradient_anim
+        ScrollView img = (ScrollView) findViewById(R.id.mainActivityLayout);
+        img.setBackgroundResource(R.drawable.gradient_anim);
+
+        // Carica lo sfondo, che è stato compilato come un oggetto AnimationDrawable
+        frameAnimation = (AnimationDrawable) img.getBackground();
+
+        // Starta l'animazione (Verrà loopata di default)
+        frameAnimation.setEnterFadeDuration(1000);
+        frameAnimation.setExitFadeDuration(3000);
+        frameAnimation.start();
 
         // Crea un Gestore ed un Thread
         CreateHandler();
